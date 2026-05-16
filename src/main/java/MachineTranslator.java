6046e2603174f2b3c7960f424f5e6850373ae3bf
Serhii <shylo20069@gmail.com>
2026-05-16 13:35:36 +0300

    remove: creating predictor for general variable

--- src/main/java/MachineTranslator.java
+++ src/main/java/MachineTranslator.java
@@ -16,9 +16,7 @@ public class MachineTranslator {
 
     private final int DECODER_LAYERS = 12; // Constant for working only with NLLB
     private final ZooModel<Encoding, NDList> ENCODER;
-    private final Predictor<Encoding, NDList> ENCODER_PREDICTOR;
     private final ZooModel<NDList, NDList> DECODER;
-    private final Predictor<NDList, NDList> DECODER_PREDICTOR;
     private final HuggingFaceTokenizer TOKENIZER;
     private static final Logger log = LoggerFactory.getLogger(MachineTranslator.class);
     public MachineTranslator(ModelConfig config){
@@ -30,10 +28,8 @@ public class MachineTranslator {
         try {
             this.DECODER = createCriteria(decoderPath, "decoder_model_merged_quantized.onnx",
                     new DecoderTranslator(), NDList.class, NDList.class).loadModel();
-            this.DECODER_PREDICTOR = DECODER.newPredictor();
             this.ENCODER = createCriteria(encoderPath, "encoder_model_quantized.onnx",
                     new EncoderTranslator(), Encoding.class, NDList.class).loadModel();
-            this.ENCODER_PREDICTOR = ENCODER.newPredictor();
             this.TOKENIZER = HuggingFaceTokenizer.newInstance(tokenizerPath);
         }catch(Exception e){
             throw new RuntimeException("Error initializing objects: ", e);
@@ -56,8 +52,9 @@ public class MachineTranslator {
         long[] attentionMask = encoding.getAttentionMask();
         long srcLangToken = TOKENIZER.encode(srcLang).getIds()[0];
         long targLangToken = TOKENIZER.encode(targLang).getIds()[0];
-        try {
-            NDList encoderOutput = ENCODER_PREDICTOR.predict(encoding);
+        try (Predictor<Encoding, NDList> encoderPredictor = ENCODER.newPredictor();
+             Predictor<NDList, NDList> decoderPredictor = DECODER.newPredictor()){
+
         }catch(Exception e){
             throw new RuntimeException("Translation error", e);
         }
