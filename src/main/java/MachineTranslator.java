446ae999cfb42f489146df6806560c6e66c81683
Serhii <shylo20069@gmail.com>
2026-05-16 11:59:46 +0300

    add: createCriteria method

--- src/main/java/MachineTranslator.java
+++ src/main/java/MachineTranslator.java
@@ -1,7 +1,9 @@
 import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer;
 import ai.djl.inference.Predictor;
 import ai.djl.ndarray.NDList;
+import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.Translator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +26,16 @@ public class MachineTranslator {
         Path encoderPath = Objects.requireNonNull(paths.get("encoder"), "Encoder not found");
         Path decoderPath = Objects.requireNonNull(paths.get("decoder"), "Decoder not found");
         Path tokenizer = Objects.requireNonNull(paths.get("tokenizer"), "Tokenizer not found");
-        
+
+    }
+    /*******
+     A method that returns a Criteria<I, O>
+     ...Params.description...
+     *******/
+    private <I, O> Criteria<I, O> createCriteria(Path modelPath, String modelName,
+                                                 Translator<I, O> translator,
+                                                 Class<I> inputType, Class<O> outputType){
+        return Criteria.builder().setTypes(inputType, outputType).optModelPath(modelPath).optModelName(modelName)
+                .optTranslator(translator).optEngine("OnnxRuntime").build();
     }
 }
