f7c0cf4b4d99771b123aac8134c139a938a10b10
Serhii <shylo20069@gmail.com>
2026-05-16 15:50:38 +0300

    add: wither-methods to DecoderInput record

--- src/main/java/DecoderInput.java
+++ src/main/java/DecoderInput.java
@@ -7,4 +7,19 @@ public record DecoderInput(
        NDArray encoderHiddenStates,
        NDList pastKeyValues,
        NDArray useCacheBranch
-) { }
+) {
+    public DecoderInput withToken(long newToken){
+        return new DecoderInput(newToken, this.encoderAttentionMask, this.encoderHiddenStates,
+                this.pastKeyValues, this.useCacheBranch);
+    }
+
+    public DecoderInput withCache(NDList newCache){
+        return new DecoderInput(currentToken, this.encoderAttentionMask, this.encoderHiddenStates,
+                newCache, this.useCacheBranch);
+    }
+
+    public DecoderInput withUseCacheBranch(NDArray newStatus){
+        return new DecoderInput(currentToken, this.encoderAttentionMask, this.encoderHiddenStates,
+                this.pastKeyValues, newStatus);
+    }
+}
