2332fba1a8d50dcd257dcb30d2ef247d453604c6
Serhii <shylo20069@gmail.com>
2026-05-16 13:06:39 +0300

    add: realization of processInput and processOutput in EcnoderTranslator

--- src/main/java/EncoderTranslator.java
+++ src/main/java/EncoderTranslator.java
@@ -1,5 +1,7 @@
 import ai.djl.huggingface.tokenizers.Encoding;
+import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.NDManager;
 import ai.djl.translate.Batchifier;
 import ai.djl.translate.Translator;
 import ai.djl.translate.TranslatorContext;
@@ -7,12 +9,19 @@ import ai.djl.translate.TranslatorContext;
 public class EncoderTranslator implements Translator<Encoding, NDList> {
     @Override
     public NDList processInput(TranslatorContext translatorContext, Encoding encoding) throws Exception {
-        return null;
+        NDManager ndManager = translatorContext.getNDManager();
+        long[] indices = encoding.getIds();
+        long[] attentionMask = encoding.getAttentionMask();
+        NDArray indicesTensor = ndManager.create(indices).reshape(1, indices.length);
+        NDArray attentionMaskTensor = ndManager.create(indices).reshape(1, attentionMask.length);
+        translatorContext.setAttachment("indices", indicesTensor);
+        translatorContext.setAttachment("attentionMask", attentionMaskTensor);
+        return new NDList(indicesTensor, attentionMaskTensor);
     }
 
     @Override
     public NDList processOutput(TranslatorContext translatorContext, NDList ndList) throws Exception {
-        return null;
+        return ndList;
     }
 
     @Override
