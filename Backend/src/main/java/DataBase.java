a7d827e5689368a88b9c8d2fe745baeebeafb260
Serhii <shylo20069@gmail.com>
2026-06-16 18:44:35 +0300

    change: path to DB

--- Backend/src/main/java/DataBase.java
+++ Backend/src/main/java/DataBase.java
@@ -10,8 +10,9 @@ public class DataBase {
     private static Statement statement;
     public DataBase(){
         HikariConfig config = new HikariConfig();
-        String currentDir = System.getProperty("usr.dir");
-        config.setJdbcUrl("jdbc:sqlite:" + currentDir + "/src/resources/data.db"); // test path
+        String userDir = System.getProperty("user.home");
+        String dbUrl = "jdbc:sqlite:" + userDir + "/Projects/Closira/Backend/src/resources/data.db";
+        config.setJdbcUrl(dbUrl); // test path
         config.setMaximumPoolSize(1);
         config.setPoolName("SQLite-pool");
         config.setAutoCommit(true);
